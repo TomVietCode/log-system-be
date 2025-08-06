c0d3ade42eaac5c86f9cdb1ef7d30afd3a919e21
TomVietCode <mcuong04.work@gmail.com>
2025-08-06 15:48:44 +0700

    update server port configuration

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,6 @@ async function bootstrap() {
   app.enableCors({  
   })
   
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 5000, '0.0.0.0');
 }
 bootstrap();
